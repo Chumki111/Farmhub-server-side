ff8f52ce63f2327311023649d8f06d794ac140db
Chumki khatun <chumkikhatun370@gmail.com>
2024-07-06 09:29:04 +0600

    all filter api create

--- index.js
+++ index.js
@@ -10,66 +10,96 @@ const port = process.env.PORT || 8000
 // N7C6kjDsWqyMLxxj
 // middleware
 const corsOptions = {
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
-    credentials: true,
-    optionSuccessStatus: 200,
+  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  credentials: true,
+  optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
+app.use(express.json())
+app.use(cookieParser())
+
+
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(process.env.DB_URL, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
   }
-  app.use(cors(corsOptions))
-  app.use(express.json())
-  app.use(cookieParser())
+});
 
-  
-  
-  
-  // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-  const client = new MongoClient(process.env.DB_URL, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    }
-  });
-  
-  async function run() {
-    try {
+async function run() {
+  try {
 
-      const servicesCollection = client.db('Farmhub').collection('services')
-      const productCollection = client.db('Farmhub').collection('products')
-       
+    const servicesCollection = client.db('Farmhub').collection('services')
+    const productCollection = client.db('Farmhub').collection('products')
 
 
-      // get all products
-      app.get('/products',async(req,res) =>{
-        const result = await productCollection.find().toArray();
-        res.send(result)
-      })
-      // all services get
-      app.get('/services',async(req,res) =>{
-        const result = await servicesCollection.find().toArray();
-        res.send(result)
-      })
-      // get single services
-      app.get('/service/:id',async(req,res) =>{
-        const id = req.params.id;
-        const result = await servicesCollection.findOne({ _id: new ObjectId(id) });
-        res.send(result)
-      })
-      // Connect the client to the server	(optional starting in v4.7)
-      await client.connect();
-      // Send a ping to confirm a successful connection
-      await client.db("admin").command({ ping: 1 });
-      console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-      // Ensures that the client will close when you finish/error
+
+    // get all products
+    app.get('/products', async (req, res) => {
+      const result = await productCollection.find().toArray();
+      res.send(result)
+    })
+    // get all products by filter
+    app.get('/allProducts', async (req, res) => {
+      const filter = req.query;
+      const query = {};
+      const options = {};
+
+      // Add category filter if available
+      if (filter.category) {
+        query.category = filter.category;
+      }
+
+      // Add color filter if available
+      if (filter.color) {
+        query.color = filter.color;
+      }
+
+      // Add unit filter if available
+      if (filter.unit) {
+        query.unit = filter.unit;
+      }
+
+      // Include sorting options if available
+      if (filter.sort) {
+        options.sort = {
+          price: filter.sort === 'asc' ? 1 : -1
+        };
+      }
+      const products = await productCollection.find(query).sort(options.sort).toArray();
+      res.send(products)
+    })
+    // all services get
+    app.get('/services', async (req, res) => {
+      const result = await servicesCollection.find().toArray();
+      res.send(result)
+    })
+    // get single services
+    app.get('/service/:id', async (req, res) => {
+      const id = req.params.id;
+      const result = await servicesCollection.findOne({ _id: new ObjectId(id) });
+      res.send(result)
+    })
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
     //   await client.close();
-    }
   }
-  run().catch(console.dir);
-  
-  app.get('/', (req, res) => {
-    res.send('Hello from Farmhub Server..')
-  })
-  
-  app.listen(port, () => {
-    console.log(`Farmhub is running on port ${port}`)
-  })
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Hello from Farmhub Server..')
+})
+
+app.listen(port, () => {
+  console.log(`Farmhub is running on port ${port}`)
+})
